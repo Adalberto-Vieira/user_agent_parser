8f7b84f276313a852c56673eace73e3de3a8590d
Adalberto Barbosa Vieira <CIANDT\adalbertov@lnb024561bhz.ciandt.global>
2023-11-05 18:40:04 -0300

    add base script functions

--- augumentor/base_script_functions.py
+++ augumentor/base_script_functions.py
@@ -1,13 +1,38 @@
+import os
+import gzip
+import json
+from typing import List
 
+INPUT_PATH = os.path.join("..", "input")
+OUTPUT_PATH = os.path.join("..", "output")
 
 
 class BaseScriptsFunctions:
+    @classmethod
+    def load_json_data(cls, file_name: str):
+        """Open and load the json data"""
+        try:
+            with gzip.open(
+                os.join(INPUT_PATH, file_name), "rt", encoding="utf-8"
+            ) as file:
+                json_data = json.loads(file.read())
+            return json_data
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {os.join(INPUT_PATH, file_name)}")
+        except json.JSONDecodeError as e:
+            raise json.JSONDecodeError(f"Error decoding JSON data: {str(e)}")
+        except Exception as e:
+            raise Exception(
+                f"An error occurred while reading the compressed JSON file: {str(e)}"
+            )
 
-
-    def load_json_data(self):
-        pass
-
-
-    def parse_agent_info(self):
-        pass
-
+    @classmethod
+    def save_processed_file(cls, file_name: str, file_data: List):
+        try:
+            with gzip.open(
+                os.join(OUTPUT_PATH.file_name), "wt", encoding="utf-8"
+            ) as file:
+                json.dump(file_data, file)
+            print(f"JSON data saved to {file_name}")
+        except Exception as e:
+            print(f"An error occurred while saving the JSON data: {str(e)}")
