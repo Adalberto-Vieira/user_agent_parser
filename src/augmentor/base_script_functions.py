7200a0813751ebd9440a1aaf30fc976555a57782
Adalberto Barbosa Vieira <CIANDT\adalbertov@lnb024561bhz.ciandt.global>
2023-11-09 08:18:12 -0300

    add base script functions

--- src/augmentor/base_script_functions.py
+++ src/augmentor/base_script_functions.py
@@ -1,38 +1,30 @@
-import os
 import gzip
 import json
 from typing import List
 
-INPUT_PATH = os.path.join("..", "..", "input")
-OUTPUT_PATH = os.path.join("..", "..", "output")
-
 
 class BaseScriptsFunctions:
     @classmethod
-    def load_json_data(cls, file_name: str):
+    def load_json_data(cls, file_path: str):
         """Open and load the json data"""
+        json_data = []
         try:
-            with gzip.open(
-                os.join(INPUT_PATH, file_name), "rt", encoding="utf-8"
-            ) as file:
-                json_data = json.loads(file.read())
+            with gzip.open(file_path, "rt", encoding="utf-8") as file:
+                for line in file:
+                    json_data.append(json.loads(line))
             return json_data
         except FileNotFoundError:
-            raise FileNotFoundError(f"File not found: {os.join(INPUT_PATH, file_name)}")
-        except json.JSONDecodeError as e:
-            raise json.JSONDecodeError(f"Error decoding JSON data: {str(e)}")
+            raise FileNotFoundError(f"File not found: {file_path}")
         except Exception as e:
             raise Exception(
                 f"An error occurred while reading the compressed JSON file: {str(e)}"
             )
 
     @classmethod
-    def save_processed_file(cls, file_name: str, file_data: List):
+    def save_processed_file(cls, file_path: str, file_data: List):
         try:
-            with gzip.open(
-                os.join(OUTPUT_PATH.file_name), "wt", encoding="utf-8"
-            ) as file:
+            with gzip.open(file_path, "wt", encoding="utf-8") as file:
                 json.dump(file_data, file)
-            print(f"JSON data saved to {file_name}")
+            print(f"JSON data saved to {file_path}")
         except Exception as e:
             print(f"An error occurred while saving the JSON data: {str(e)}")
