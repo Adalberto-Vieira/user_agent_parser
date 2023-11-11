36a3f0b071bd663a1a81a4a1396109ceb9dbdf11
Adalberto Barbosa Vieira <CIANDT\adalbertov@lnb024561bhz.ciandt.global>
2023-11-10 21:57:51 -0300

    refactor

--- src/main.py
+++ src/main.py
@@ -6,12 +6,5 @@ OUTPUT_PATH = os.path.join("..", "output")
 if __name__ == "__main__":
     """ """
 
-    # clear the folder output from previous runs
-    files = os.listdir(OUTPUT_PATH)
-    for file in files:
-        file_path = os.path.join(OUTPUT_PATH, file)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-
-    script = augmentor.UserAgentAugmentor()
-    script.run()
+    ua_augmentor = augmentor.UserAgentAugmentor()
+    ua_augmentor.run()
