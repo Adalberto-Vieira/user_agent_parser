791e8145a306ca7f42601ea85ed2e0af885ccc3d
Adalberto Barbosa Vieira <CIANDT\adalbertov@lnb024561bhz.ciandt.global>
2023-11-10 21:56:12 -0300

    add utils file

--- src/augmentor/file_utils.py
+++ src/augmentor/file_utils.py
@@ -1,9 +1,10 @@
+import os
 import gzip
 import json
 from typing import List
 
 
-class BaseScriptsFunctions:
+class FileUtils:
     @classmethod
     def load_json_data(cls, file_path: str):
         """Open and load the json data"""
@@ -21,10 +22,22 @@ class BaseScriptsFunctions:
             )
 
     @classmethod
-    def save_processed_file(cls, file_path: str, file_data: List):
+    def save_processed_file(cls, file_path: str, file_name: str, file_data: List):
         try:
-            with gzip.open(file_path, "wt", encoding="utf-8") as file:
+            cls._clear_output_folder(file_path)
+            with gzip.open(
+                os.path.join(file_path, file_name), "wt", encoding="utf-8"
+            ) as file:
                 json.dump(file_data, file)
             print(f"JSON data saved to {file_path}")
         except Exception as e:
             print(f"An error occurred while saving the JSON data: {str(e)}")
+
+    @classmethod
+    def _clear_output_folder(cls, output_path):
+        # clear the folder output from previous runs
+        files = os.listdir(output_path)
+        for file in files:
+            file_path = os.path.join(output_path, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
